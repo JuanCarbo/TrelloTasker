4f8149b2d2f4099684862b3cfc97395f1049db57
Juan Carbó <juan.carbo@globallogic.com>
2020-08-21 23:18:43 -0300

    Added TrelloWebhookController

---
+++ src/TrelloTasker.Api/Controllers/TrelloWebhookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrelloTasker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrelloWebhookController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> OnWebhookCallBack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
