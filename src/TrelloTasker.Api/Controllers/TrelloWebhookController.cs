37a07f927af1cd5be2af3e6d20fd3b492055bea8
Juan Carbó <juan.carbo@globallogic.com>
2020-08-22 00:33:42 -0300

    Added a webhook controller for trello webhooks.

--- src/TrelloTasker.Api/Controllers/TrelloWebhookController.cs
+++ src/TrelloTasker.Api/Controllers/TrelloWebhookController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TrelloTasker.Model.DTO;
 
 namespace TrelloTasker.Api.Controllers
 {
@@ -9,8 +11,14 @@ namespace TrelloTasker.Api.Controllers
     public class TrelloWebhookController : ControllerBase
     {
         [HttpPost]
-        public async Task<IActionResult> OnWebhookCallBack()
+        public async Task<IActionResult> OnWebhookCallBack(TrelloWebhookDTO trelloWebhookDTO)
         {
+            //Este metodo inicia cuando se cambia algo en trello, y nos avisa por acá.
+            if (trelloWebhookDTO.action.data.card.name.EndsWith("Bi-daily"))
+            {
+                throw new NotImplementedException();
+            }
+            return new OkObjectResult(trelloWebhookDTO);
             throw new NotImplementedException();
         }
     }
